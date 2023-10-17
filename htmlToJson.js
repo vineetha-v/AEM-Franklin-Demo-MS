870bc5bc3d889b3c71d4ed51571ccfc19f56d710
vineetha-v <vineetha.venugopal@pfizer.com>
2023-10-18 01:17:04 +0530

    Modified parsing

---
+++ htmlToJson.js
@@ -0,0 +1,31 @@
+// var hObj = require('html-to-json');
+
+import pkg from 'htm-to-json';
+
+const { convert_html_to_json } = pkg;
+
+import { readFile, writeFileSync } from 'fs';
+
+readFile('data/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        // console.log(data);
+        convert_html_to_json(data, function (err, dataFinal) {
+            if (err) throw err;
+            else {
+                console.log(Object.keys(dataFinal));
+                writeFileSync('parsed_data.json', JSON.stringify(dataFinal, null, 2), function (err) {
+                    if (err) throw err;
+                    console.log('successfully saved file')
+                });
+            }
+        });
+    }
+
+
+});
+
+
+
